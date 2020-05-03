a604014fef8f4a299eb38cc795caac595db51c19
zewelor <zewelor@gmail.com>
2020-05-03 10:48:57 +0200

    Add lte restart

---
+++ files/summerhouse_rack/root/restart-router.sh
@@ -0,0 +1,14 @@
+#!/bin/ash
+# This file is responsible for restarting the router using reboot command.
+# There is a stratery to write few lines into the log, so that grepping last lines
+# returns less occurences of word OFFLINE. Too many occurences actually run this script.
+
+LOG_FILE="/tmp/log.txt"
+
+echo "$(date) > TOO MANY OFFLINE TRYOUTS" >> $LOG_FILE
+echo "$(date) > GOING TO REBOOT NOW" >> $LOG_FILE
+echo "$(date) > NOW!" >> $LOG_FILE
+echo "$(date) > SORRY FOR ANY INCONVENIENCE." >> $LOG_FILE
+
+echo "SH REBOOT"
+reboot
